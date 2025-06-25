025164279304deb1cf53a770cddec98857dfb606
EugeneSchoolnik <kn23-ye.piskalenko@nubip.edu.ua>
2025-06-25 09:21:15 +0300

    Merge feature/logger into feature/authorization

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"log"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
 	"url-shortener/internal/database"
+	"url-shortener/internal/lib/logger/sl"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	config := config.MustLoad()
 	log.Printf("config loaded: %v\n", config)
 
+	log := setupLogger(config.Env)
+	log.Info("starting url-shortener", slog.String("env", config.Env))
+	log.Debug("debug messages are enabled")
+
 	db, err := database.New(&config.Postgres)
 	if err != nil {
-		log.Fatalf("failed to init database: %s", err.Error())
+		log.Error("failed to init database: %s", sl.Err(err))
 	}
-	log.Println("database initialized")
+	log.Info("database initialized")
 
 	_ = db
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
